24b98efddc6558a6b22aab3caa5ef2ee953e5dd0
Amit Pandey <amit.pandey.iitm@gmail.com>
2017-02-17 17:18:00 +0530

    CR:AMIT PANDEY initial vue applcication crud

---
+++ backend/Events.js
@@ -0,0 +1,24 @@
+module.exports = [
+  {
+    id: 1,
+    title: 'Pravish',
+    detail: 'Skype with guys from NASA',
+    date: '2015-12-12'
+  },
+  {
+    id: 2,
+    title: 'Amit',
+    detail: 'ROR Developer',
+    date: '2016-1-1'
+  },
+  {
+    id: 3,
+    title: 'Rakesh',
+    detail: 'ROR Developer',
+    date: '2016-1-1'
+  },
+  {
+    id: 4,
+    title: 'Yogendra'
+  }
+];
